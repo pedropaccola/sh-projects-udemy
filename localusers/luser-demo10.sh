e7a3d80c697245a7c44bf5e5b3748cbb394c78aa
Pedro Paccola <paccolapedro@gmail.com>
2023-05-23 21:14:39 -0300

    Functions

---
+++ localusers/luser-demo10.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function log {
+  # This function sends a message to syslog and to STDOUT if VERBOSE is true
+  local MESSAGE="${*}"
+  if [[ "${VERBOSE}" = 'true' ]]
+  then
+    echo "${MESSAGE}"
+  fi
+  logger -t "$(basename "${0}")" "${MESSAGE}"
+}
+
+function backup_file {
+  #This function creates a backup of a file. Returns non-zero status on error.
+  local FILE="${1}"
+
+  # Make sure file exists.
+  if [[ -f "${FILE}" ]]
+  then
+    local BACKUP_FILE
+    BACKUP_FILE="/var/tmp/$(basename "${FILE}").$(date +%F-%N)"
+    log "Backing up ${FILE} to ${BACKUP_FILE}"
+
+    # The exit status of the function will be the exit status of the cp command.
+    cp -p "${FILE}" "${BACKUP_FILE}"
+  else
+    # The file does not exist, so return a non-zero exit status
+    return 1
+  fi
+}
+
+readonly VERBOSE='true'
+log 'Hello!' 
+log 'This is fun!'
+
+# Make a decision based on the exit status of the function.
+if backup_file '/etc/passwd' # Running a command and then checking it's exit code with $? is redundant
+then
+  log 'File backup succeeded!'
+else
+  log 'File backup failed!'
+  exit 1
+fi
+
